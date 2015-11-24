8e51887bafdb5d46bf3e98329dbe2580056a29e7
Sergey Chikuyonok <serge.che@gmail.com>
2015-11-24 11:47:37 +0300

    Override parseFile method

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 module.exports = require('./build/libxslt');
+var binding = require('bindings')('node-libxslt');
+
+// override `parseFile` method with the one that properly reads stylesheet
+// from file and retains file context
+exports.parseFile = function(sourcePath, options, callback) {
+	if (typeof options === 'function') {
+		callback = options;
+		options = {};
+	}
+
+	try {
+		var stylesheet = exports.parse(binding.readXmlFile(sourcePath, options));
+		if (typeof callback === 'function') {
+			callback(null, stylesheet);
+		}
+		return stylesheet;
+	} catch (err) {
+		return callback(err);
+	}
+};
